18aeb5c23c40532049194ae0ec30da287f17ab98
LionelMv <mvangicode@gmail.com>
2024-08-13 23:32:36 +0300

    feat: Add *args and **kwargs
    Added *args and **kwargs in constructor method.
    if kwargs:
     - change type of created_at and updated_at to datetime.
     - remove __class__ key and value.

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,19 @@ class BaseModel:
     Base Model Class
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k in ["created_at", "updated_at"]:
+                    setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif k != "__class__":
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
 
     def __str__(self):
         """String representation of the BaseModel"""
@@ -28,8 +36,7 @@ class BaseModel:
     def to_dict(self):
         """Returns a dictionary representation of an instance"""
         dict = self.__dict__
-        dict["__class__"] = __class__.__name__
+        dict["__class__"] = self.__class__.__name__
         dict["created_at"] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         dict["updated_at"] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         return dict
-
