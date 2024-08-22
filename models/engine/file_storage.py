d3e7f6bef0304fcb2c3c1603ca81252e131cecec
LionelMv <mvangicode@gmail.com>
2024-08-22 19:50:52 +0300

    feat: Create a FileStorage class
    Create a FileStorage class with this methods:
    - all(): Returns all objects.
    - new(): Adds a new object to storage dictionary
    - save(): Serializes objects to JSON and saves them to the file.
    - reload(): Deserializes objects from the JSON file.

---
+++ models/engine/file_storage.py
@@ -0,0 +1,60 @@
+#!usr/bin/python3
+"""
+File Storage
+"""
+import json
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns all objects"""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Adds new object to storage dictionary"""
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """ Serializes __objects to the JSON file (__file_path)"""
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(dict, f)
+
+    def reload(self):
+        """Deserializes to __objects the JSON file (__file_path)"""
+        from models.base_model import BaseModel
+        # from models.user import User
+        # from models.place import Place
+        # from models.state import State
+        # from models.city import City
+        # from models.amenity import Amenity
+        # from models.review import Review
+
+        """classes = {
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }"""
+        classes = {
+            'BaseModel': BaseModel
+        }
+
+        try:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                dict = json.load(f)
+            
+            for key, val in dict.items():
+                # class_name = key.split(".")[0]
+                # obj = eval(class_name)(**val)
+                # or
+                # key = BaseModel.id
+                # obj = BaseModel(**kwargs)
+                # obj = classes["BaseModel"](**val)
+                obj = classes[val["__class__"]](**val)
+                self.all()[key] = obj
+        except FileNotFoundError:
+            pass
