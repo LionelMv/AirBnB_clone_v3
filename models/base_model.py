dfffc03c3a19b0d8a977e55977209fc2fe34a7c5
LionelMv <mvangicode@gmail.com>
2024-08-22 19:39:33 +0300

    feat: Add new() and save() methods from FileStorage
    Added this methods to the BaseModel class from FileStorage
    - storage.new(self): Added to the constructor method when creating new
      instances.
    - storage.save(): Added to save() method.

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Base Model Class
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -23,6 +24,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
 
     def __str__(self):
@@ -32,6 +34,7 @@ class BaseModel:
     def save(self):
         """Update the updated time"""
         self.updated_at = datetime.now()
+        storage.save()
     
     def to_dict(self):
         """Returns a dictionary representation of an instance"""
